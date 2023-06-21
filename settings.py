af30b5564b3202a9f87fa2bb5a9e3606051f49ae
memoran1998 <memoran1998@gmail.com>
2023-06-21 10:41:53 -0400

    added ship movement

--- settings.py
+++ settings.py
@@ -12,4 +12,7 @@ class Setting:
         #screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (100, 150, 200)
+        
+        #Ship settings
+        self.ship_speed = 1.5
